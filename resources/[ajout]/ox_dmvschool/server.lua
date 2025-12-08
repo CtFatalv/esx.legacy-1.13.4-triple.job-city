ccc69b9468cbd6b377187dd9f01aa35d1c5a5a58
CtFatalv <server.host.contact@gmail.com>
2025-12-08 20:00:42 +0100

    Legacy City

--- resources/[ajout]/ox_dmvschool/server.lua
+++ resources/[ajout]/ox_dmvschool/server.lua
@@ -1,33 +1,5 @@
 local ESX = exports.es_extended:getSharedObject()
 
-AddEventHandler('onResourceStart', function(resource)
-    if resource == GetCurrentResourceName() then
-        for k,v in pairs(Config.License) do
-            local result =  MySQL.Sync.fetchAll("SELECT * FROM licenses WHERE type = @type", {
-                  ['@type'] = v.id,
-            }) 
-            if result == nil or result[1] == nil then
-                MySQL.Async.execute("INSERT INTO licenses (type, label) VALUES (@type, @label)", {
-                    ['@type'] = v.id,
-                    ['@label'] = v.label,
-                }) 
-            end
-        end
-
-        for k,v in pairs(Config.License) do
-            local result =  MySQL.Sync.fetchAll("SELECT * FROM licenses WHERE type = @type", {
-                  ['@type'] = v.id.."dmv",
-            }) 
-            if result == nil or result[1] == nil then
-                MySQL.Async.execute("INSERT INTO licenses (type, label) VALUES (@type, @label)", {
-                    ['@type'] = v.id.."dmv",
-                    ['@label'] = v.label.." DMV",
-                }) 
-            end
-        end
-    end
-end)
-
 ESX.RegisterServerCallback('ricky-dmv:getData', function(source, cb)
     local licenses = Config.License
     local xPlayer = ESX.GetPlayerFromId(source)
