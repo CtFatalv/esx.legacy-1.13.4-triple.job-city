116d3b280af0812c4816749e0df7fa8a12a61cec
CtFatalv <server.host.contact@gmail.com>
2025-12-09 12:39:20 +0100

    Legacy City

--- resources/[ajout]/ox_dmvschool/config.lua
+++ resources/[ajout]/ox_dmvschool/config.lua
@@ -94,343 +94,163 @@ Config.PracticeCoords = {
 Config.Question = {
     [1] = {
         {
-            label = "Quelle est la couleur du panneau d'interdiction ?",
+            label = "Quelle est la couleur principale d’un panneau d’interdiction ?",
             options = {
-                {
-                    label = "Rouge",
-                    correct = true
-                },
-                {
-                    label = "Bleu",
-                    correct = false
-                },
-                {
-                    label = "Jaune",
-                    correct = false
-                }
+                { label = "Rouge", correct = true },
+                { label = "Bleu", correct = false },
+                { label = "Jaune", correct = false }
             }
         },
         {
-            label = "Que signifie le panneau en forme de triangle ?",
+            label = "Que signifie un panneau triangulaire bordé de rouge ?",
             options = {
-                {
-                    label = "Intersection en T",
-                    correct = false
-                },
-                {
-                    label = "Cédez le passage",
-                    correct = true
-                },
-                {
-                    label = "Sens unique",
-                    correct = false
-                }
+                { label = "Intersection en T", correct = false },
+                { label = "Danger / Attention", correct = true },
+                { label = "Sens unique", correct = false }
             }
         },
         {
             label = "Que signifie une ligne continue au milieu de la route ?",
             options = {
-                {
-                    label = "Dépassement autorisé",
-                    correct = false
-                },
-                {
-                    label = "Dépassement interdit",
-                    correct = true
-                },
-                {
-                    label = "Dépassement autorisé uniquement à droite",
-                    correct = false
-                }
+                { label = "Dépassement autorisé", correct = false },
+                { label = "Dépassement interdit", correct = true },
+                { label = "Dépassement autorisé uniquement à droite", correct = false }
             }
         },
         {
             label = "Quelle est la limite de vitesse en zone résidentielle ?",
             options = {
-                {
-                    label = "50 km/h",
-                    correct = true
-                },
-                {
-                    label = "80 km/h",
-                    correct = false
-                },
-                {
-                    label = "120 km/h",
-                    correct = false
-                }
+                { label = "50 km/h", correct = true },
+                { label = "80 km/h", correct = false },
+                { label = "120 km/h", correct = false }
             }
         },
         {
-            label = "Que signifie un panneau de danger avec un appareil photo ?",
+            label = "Que signifie un panneau avertissant d’un radar automatique (symbole appareil photo) ?",
             options = {
-                {
-                    label = "Zone de stationnement",
-                    correct = false
-                },
-                {
-                    label = "Zone d'interdiction de stationner",
-                    correct = false
-                },
-                {
-                    label = "Zone d'interdiction de photographier",
-                    correct = true
-                }
+                { label = "Zone de stationnement", correct = false },
+                { label = "Zone d'interdiction de stationner", correct = false },
+                { label = "Présence d’un radar automatique", correct = true }
             }
         },
         {
             label = "Quelle est la distance minimale de sécurité à maintenir ?",
             options = {
-                {
-                    label = "1 mètre",
-                    correct = false
-                },
-                {
-                    label = "2 secondes de distance",
-                    correct = true
-                },
-                {
-                    label = "0,5 mètre",
-                    correct = false
-                }
+                { label = "1 mètre", correct = false },
+                { label = "2 secondes de distance", correct = true },
+                { label = "0,5 mètre", correct = false }
             }
         },
         {
-            label = "Que doit faire un conducteur à un passage à niveau avec barrières ouvete ?",
+            label = "Que doit faire un conducteur à un passage à niveau avec barrières ouvertes ?",
             options = {
-                {
-                    label = "Accélérer pour passer avant que les barrières ne se ferment complètement",
-                    correct = false
-                },
-                {
-                    label = "Traverser seulement si aucun train n'arrive",
-                    correct = true
-                },
-                {
-                    label = "Klaxonner et avancer",
-                    correct = false
-                }
+                { label = "Accélérer pour passer avant qu’elles ne se ferment", correct = false },
+                { label = "Traverser seulement si aucun train n'arrive", correct = true },
+                { label = "Klaxonner et avancer", correct = false }
             }
         },
         {
             label = "Que signifie un panneau stop ?",
             options = {
-                {
-                    label = "Cédez le passage",
-                    correct = false
-                },
-                {
-                    label = "Obligation de s'arrêter",
-                    correct = true
-                },
-                {
-                    label = "Sens unique",
-                    correct = false
-                }
+                { label = "Cédez le passage", correct = false },
+                { label = "Obligation de s'arrêter", correct = true },
+                { label = "Sens unique", correct = false }
             }
         },
         {
             label = "Que signifie un panneau bleu avec des piétons ?",
             options = {
-                {
-                    label = "Voie réservée aux vélos",
-                    correct = false
-                },
-                {
-                    label = "Voie réservée aux piétons",
-                    correct = true
-                },
-                {
-                    label = "Voie réservée aux transports publics",
-                    correct = false
-                }
+                { label = "Voie réservée aux vélos", correct = false },
+                { label = "Voie réservée aux piétons", correct = true },
+                { label = "Voie réservée aux transports publics", correct = false }
             }
         },
         {
-            label = "Que signifie un panneau avec une croix de Saint-André (X rouge sur fond blanc) ?",
+            label = "Que signifie une croix de Saint-André (X rouge sur fond blanc) ?",
             options = {
-                {
-                    label = "Zone à accès limité",
-                    correct = true
-                },
-                {
-                    label = "Zone de stationnement autorisé",
-                    correct = false
-                },
-                {
-                    label = "Zone piétonne",
-                    correct = false
-                }
+                { label = "Passage à niveau sans barrières", correct = true },
+                { label = "Zone de stationnement autorisé", correct = false },
+                { label = "Zone piétonne", correct = false }
             }
         },
         {
             label = "Que signifie un panneau d'interdiction d'accès ?",
             options = {
-                {
-                    label = "Cédez le passage",
-                    correct = false
-                },
-                {
-                    label = "Interdiction de circuler",
-                    correct = true
-                },
-                {
-                    label = "Dépassement obligatoire",
-                    correct = false
-                }
+                { label = "Cédez le passage", correct = false },
+                { label = "Interdiction de circuler dans ce sens", correct = true },
+                { label = "Dépassement obligatoire", correct = false }
             }
         },
         {
             label = "Que doit faire un conducteur à un carrefour sans signalisation ?",
             options = {
-                {
-                    label = "Accélérer pour traverser rapidement",
-                    correct = false
-                },
-                {
-                    label = "S'arrêter, céder le passage et avancer avec prudence",
-                    correct = true
-                },
-                {
-                    label = "Klaxonner pour prévenir les autres conducteurs",
-                    correct = false
-                }
+                { label = "Accélérer pour traverser rapidement", correct = false },
+                { label = "Céder le passage à droite et avancer avec prudence", correct = true },
+                { label = "Klaxonner pour prévenir les autres", correct = false }
             }
         },
         {
-            label = "Que signifie un panneau de virage dangereux à gauche ?",
+            label = "Que signifie un panneau annonçant un virage dangereux à gauche ?",
             options = {
-                {
-                    label = "Proximité d'une aire de repos",
-                    correct = false
-                },
-                {
-                    label = "Présence d'une intersection",
-                    correct = false
-                },
-                {
-                    label = "Présence d'un virage dangereux à gauche",
-                    correct = true
-                }
+                { label = "Proximité d'une aire de repos", correct = false },
+                { label = "Présence d'une intersection", correct = false },
+                { label = "Virage dangereux à gauche", correct = true }
             }
         },
         {
             label = "Que signifie un panneau de fin d'interdiction de dépasser ?",
             options = {
-                {
-                    label = "Dépassement autorisé",
-                    correct = true
-                },
-                {
-                    label = "Interdiction de stationner",
-                    correct = false
-                },
-                {
-                    label = "Fin de l'autoroute",
-                    correct = false
-                }
+                { label = "Dépassement autorisé", correct = true },
+                { label = "Interdiction de stationner", correct = false },
+                { label = "Fin de l'autoroute", correct = false }
             }
         },
         {
             label = "Que signifie un panneau de passage piéton ?",
             options = {
-                {
-                    label = "Passage réservé uniquement aux vélos",
-                    correct = false
-                },
-                {
-                    label = "Interdiction de traverser pour les piétons",
-                    correct = false
-                },
-                {
-                    label = "Endroit où les piétons peuvent traverser en sécurité",
-                    correct = true
-                }
+                { label = "Passage réservé aux vélos", correct = false },
+                { label = "Interdiction de traverser", correct = false },
+                { label = "Passage pour piétons", correct = true }
             }
         },
         {
             label = "Que signifie un panneau de fin de zone à circulation limitée ?",
             options = {
-                {
-                    label = "Début d'une zone à circulation limitée",
-                    correct = false
-                },
-                {
-                    label = "Fin d'une zone de stationnement",
-                    correct = false
-                },
-                {
-                    label = "Fin de la zone avec des restrictions d'accès",
-                    correct = true
-                }
+                { label = "Début de zone limitée", correct = false },
+                { label = "Fin de la zone de stationnement", correct = false },
+                { label = "Fin des restrictions d'accès", correct = true }
             }
         },
         {
             label = "Que signifie un panneau d'interdiction aux véhicules motorisés ?",
             options = {
-                {
-                    label = "Cédez le passage",
-                    correct = false
-                },
-                {
-                    label = "Interdiction uniquement pour les camions",
-                    correct = false
-                },
-                {
-                    label = "Interdiction pour tous les véhicules motorisés",
-                    correct = true
-                }
+                { label = "Cédez le passage", correct = false },
+                { label = "Interdiction uniquement pour les camions", correct = false },
+                { label = "Interdiction pour tous les véhicules motorisés", correct = true }
             }
         },
         {
             label = "Que doit faire un conducteur à un feu jaune clignotant ?",
             options = {
-                {
-                    label = "Accélérer pour passer avant le changement de feu",
-                    correct = false
-                },
-                {
-                    label = "S'arrêter seulement s'il y a des véhicules dans l'intersection",
-                    correct = true
-                },
-                {
-                    label = "Continuer sans ralentir",
-                    correct = false
-                }
+                { label = "Accélérer pour passer", correct = false },
+                { label = "Avancer avec prudence en cédant la priorité", correct = true },
+                { label = "Continuer sans ralentir", correct = false }
             }
         },
         {
             label = "Que signifie un panneau d'interdiction aux piétons ?",
             options = {
-                {
-                    label = "Interdiction uniquement aux cyclistes",
-                    correct = false
-                },
-                {
-                    label = "Interdiction uniquement aux piétons",
-                    correct = true
-                },
-                {
-                    label = "Obligation de traverser uniquement en vélo",
-                    correct = false
-                }
+                { label = "Interdiction aux cyclistes", correct = false },
+                { label = "Interdiction aux piétons", correct = true },
+                { label = "Obligation de passer en vélo", correct = false }
             }
         },
         {
-            label = "Que signifie un panneau d'interdiction aux véhicules tractant une remorque ?",
+            label = "Que signifie un panneau interdisant les véhicules tractant une remorque ?",
             options = {
-                {
-                    label = "Interdiction uniquement aux camping-cars",
-                    correct = false
-                },
-                {
-                    label = "Interdiction aux véhicules avec remorque",
-                    correct = true
-                },
-                {
-                    label = "Obligation de tracter une remorque",
-                    correct = false
-                }
+                { label = "Interdiction aux camping-cars", correct = false },
+                { label = "Interdiction aux véhicules avec remorque", correct = true },
+                { label = "Obligation de tracter une remorque", correct = false }
             }
         }
     }
